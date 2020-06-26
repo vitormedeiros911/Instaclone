d460d1903144e36c1bbb130069d26c17629168e8
vitormedeiros911 <vitormedeiros911@outlook.com>
2020-06-25 23:32:16 -0300

    Adiciona o projeto

---
+++ README.md
@@ -0,0 +1,16 @@
+# Instaclone
+It's a clone of the feed from the instagram app
+
+## Some Used Features
+* React-native,
+* Expo,
+* Styled-components (First time that i am using it),
+* Json-server to provide fake data,
+* FlatList (With animation).
+
+## Run json-server
+(on terminal server)
+
+```
+yarn json-server --host your-ip-address fake-api.json -d -w
+```
---
+++ src/routes.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Image } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import logo from "../assets/instagram.png";
+import Feed from "./pages/Feed";
+
+const Routes = createAppContainer(
+  createStackNavigator(
+    {
+      Feed,
+    },
+    {
+      defaultNavigationOptions: () => ({
+        headerTitle: () => <Image source={logo} />,
+        headerTitleAlign: "center",
+        headerStyle: {
+          backgroundColor: "#f5f5f5",
+        },
+      }),
+    }
+  )
+);
+
+export default Routes;
